a971d4782a921308fcedfe906c04984d4e0b1865
Drack112 <drack@localhost.localdomain>
2022-12-21 12:32:13 -0300

    Improve formatting

--- src/modules.config.ts
+++ src/modules.config.ts
@@ -1,31 +1,32 @@
-import { Logger } from "@nestjs/common";
-import { ConfigModule, ConfigService } from "@nestjs/config";
-import { RedisModule } from "./redis/redis.module";
+import { Logger } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+import { RedisModule } from './redis/redis.module';
 
 export const CustomRedisModule = RedisModule.registerAsync({
   imports: [ConfigModule],
   useFactory: async (ConfigService: ConfigService) => {
-    const logger = new Logger("RedisModule")
+    const logger = new Logger('RedisModule');
 
     return {
       connectionOptions: {
-        host: ConfigService.get("REDIS_HOST"),
-        port: ConfigService.get("REDIS_PORT"),
+        host: ConfigService.get('REDIS_HOST'),
+        port: ConfigService.get('REDIS_PORT'),
       },
       onClientReady: (client) => {
-        logger.log("Redis client ready");
-        
-        client.on("error", (err) => {
-          logger.error("Redis client Error: ", err)
-        })
+        logger.log('Redis client ready');
 
-        client.on("connect", () => {
+        client.on('error', (err) => {
+          logger.error('Redis client Error: ', err);
+        });
+
+        client.on('connect', () => {
           logger.log(
-             `Connected to redis on ${client.options.host}:${client.options.port}`,
-          )
-        })
-      }
-    }
+            `Connected to redis on ${client.options.host}:${client.options.port}`,
+          );
+        });
+      },
+    };
   },
-  inject: [ConfigService]
-})
+  inject: [ConfigService],
+});
