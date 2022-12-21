542f0c56a0bb5875e9690d38cf7001b15968f874
Drack112 <drack@localhost.localdomain>
2022-12-21 15:31:36 -0300

    Routes and services on polls module

--- src/polls/polls.service.ts
+++ src/polls/polls.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, Logger } from '@nestjs/common';
+
 import { createPollID, createuserID } from 'utils/generate-nano-id';
 import { PollsRepository } from './polls.repository';
 import { CreatePollFields, JoinPollFields, RejoinPollFields } from './types';
@@ -7,32 +8,48 @@ import { CreatePollFields, JoinPollFields, RejoinPollFields } from './types';
 export class PollsService {
   private readonly logger = new Logger(PollsService.name);
   constructor(private readonly pollsRepository: PollsRepository) {}
-
   async createPoll(fields: CreatePollFields) {
     const pollID = createPollID();
     const userID = createuserID();
 
     const createdPoll = await this.pollsRepository.createPoll({
       ...fields,
-      userID,
       pollID,
+      userID,
     });
+
+    // TODO - create an accessToken based off of pollID and userID
 
     return {
       poll: createdPoll,
+      // accessToken
     };
   }
 
   async joinPoll(fields: JoinPollFields) {
     const userID = createuserID();
 
+    this.logger.debug(
+      `Fetching poll with ID: ${fields.pollID} for user with ID: ${userID}`,
+    );
+
+    const joinedPoll = await this.pollsRepository.getPoll(fields.pollID);
+
+    // TODO - create access Token
+
     return {
-      ...fields,
-      userID,
+      poll: joinedPoll,
+      // accessToken: signedString,
     };
   }
 
   async rejoinPoll(fields: RejoinPollFields) {
-    return fields;
+    this.logger.debug(
+      `Rejoining poll with ID: ${fields.pollID} for user with ID: ${fields.userID} with name: ${fields.name}`,
+    );
+
+    const joinedPoll = await this.pollsRepository.addParticipant(fields);
+
+    return joinedPoll;
   }
 }
