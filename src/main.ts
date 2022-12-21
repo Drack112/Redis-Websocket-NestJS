ca2ed34cc0a71ae49136531c235eb7f96c52db78
Drack <drack@localhost.localdomain>
2022-12-21 12:13:34 -0300

    NestJs generated app and setup config

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const logger: Logger = new Logger('Main (main.ts)');
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: [
+        'http://localhost:8000',
+        /^http:\/\/192\.168\.1\.([1-9]|[1-9]\d):8080$/,
+      ],
+    },
+  });
+
+  const configService = app.get(ConfigService);
+  const port = parseInt(configService.get('PORT'));
+
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(3000);
+
+  logger.log('Server running on port: ', port);
+}
+bootstrap();
