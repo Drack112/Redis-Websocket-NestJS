6528342c3191e673c86fa90635fc5a9491f682a0
Drack112 <drack@localhost.localdomain>
2022-12-22 23:03:56 -0300

    Gateway to work with new modules

--- src/polls/polls.gateway.ts
+++ src/polls/polls.gateway.ts
@@ -175,10 +175,26 @@ export class PollsGateway
       rankings,
     });
 
-    // an enhancement might be to not send ranking data to clients,
-    // but merely a list of the participants who have voted since another
-    // participant getting this data could lead to cheating
-    // we may add this while working on the client
     this.io.to(client.pollID).emit('poll_updated', updatedPoll);
+  }
+
+  @UseGuards(GatewayAdminGuard)
+  @SubscribeMessage('close_poll')
+  async closePoll(@ConnectedSocket() client: SocketWithAuth): Promise<void> {
+    this.logger.debug(`Closing poll: ${client.pollID} and computing results`);
+
+    const updatedPoll = await this.pollsService.computeResults(client.pollID);
+
+    this.io.to(client.pollID).emit('poll_updated', updatedPoll);
+  }
+
+  @UseGuards(GatewayAdminGuard)
+  @SubscribeMessage('cancel_poll')
+  async cancelPoll(@ConnectedSocket() client: SocketWithAuth): Promise<void> {
+    this.logger.debug(`Cancelling poll with id: "${client.pollID}"`);
+
+    await this.pollsService.cancelPoll(client.pollID);
+
+    this.io.to(client.pollID).emit('poll_cancelled');
   }
 }
